75ac3d3264ecc257bda3b04d0591733a12fbb754
theluney <78725873+theluney@users.noreply.github.com>
2021-03-26 16:34:24 +0600

    CV
    cv

---
+++ Altinay port/js/main.js
@@ -0,0 +1,143 @@
+function gmail(){
+    document.getElementById('gmail').style.color = "#fff";
+}
+function gmailClose(){
+    document.getElementById('gmail').style.color = "#000";
+}
+function insta(){
+    document.getElementById('instagram').style.color = "#fff";
+}
+function instaClose(){
+    document.getElementById('instagram').style.color = "#000";
+}
+function whatsapp(){
+    document.getElementById('whatsapp').style.color = "#fff";
+}
+function whatsappClose(){
+    document.getElementById('whatsapp').style.color = "#000";
+}
+
+$("#about").click(function() {
+    $('html, body').animate({
+        scrollTop: $('.about').offset().top
+    }, 500);   
+});
+
+
+$("#down").click(function() {
+    $('html, body').animate({
+        scrollTop: $('.about').offset().top
+    }, 500);   
+});
+
+$("#contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $('.contact').offset().top
+    }, 500);   
+});
+
+
+$("#portfolio").click(function() {
+    $('html, body').animate({
+        scrollTop: $('.portfolio').offset().top
+    }, 500);   
+});
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+    constructor(el) {
+      this.el = el
+      this.chars = '!<>-_\\/[]{}—=+*^?#________'
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+  
+  // ——————————————————————————————————————————————————
+  // Example
+  // ——————————————————————————————————————————————————
+  
+  const phrases = [
+    'BORANBAY ALTYNAY',
+  ]
+  
+  const el = document.querySelector('.text')
+  const fx = new TextScramble(el)
+  
+  let counter = 0
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 800)
+    })
+    counter = (counter + 1) % phrases.length
+  }
+  
+  next()
+
+  $(function(){
+    $('.minimized').click(function(event) {
+      var i_path = $(this).attr('src');
+      $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+      $('#magnify').css({
+       left: ($(document).width() - $('#magnify').outerWidth())/2,
+       // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
+              top: ($(window).height() - $('#magnify').outerHeight())/2
+     });
+      $('#overlay, #magnify').fadeIn('fast');
+    });
+    
+    $('body').on('click', '#close-popup, #overlay', function(event) {
+      event.preventDefault();
+      $('#overlay, #magnify').fadeOut('fast', function() {
+        $('#close-popup, #magnify, #overlay').remove();
+      });
+    });
+  });
